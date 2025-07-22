2e1021b83c77ac93d3288335eb471fd3f8360dbd
Rakha Viantoni <rakhaviantoni@Rakhas-MacBook-Pro.local>
2025-07-22 16:25:21 +0700

    huft

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,13 @@ import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
-import node from '@astrojs/node';
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
   output: 'server',
-  adapter: node({
-    mode: 'standalone'
-  }),
+  adapter: netlify(),
   site: 'https://inglify.rakhaviantoni.com',
   base: '/',
   vite: {
