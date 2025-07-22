7c6ab8562603fa0b8bac8631d99d374ba01cbd6b
Rakha Viantoni <rakhaviantoni@Rakhas-MacBook-Pro.local>
2025-07-22 16:05:18 +0700

    run ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,15 @@ import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
   output: 'server',
+  adapter: node({
+    mode: 'standalone'
+  }),
   site: 'https://inglify.rakhaviantoni.com',
   base: '/',
   vite: {
